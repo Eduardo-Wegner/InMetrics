b43354087157139f82efdb28b5e4d286be32711a
Eduardo.Wegner <eduwegner86@gmail.com>
2023-06-09 12:23:27 -0300

    feat(XXXX): added .env, diagram files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,20 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StatementsModule } from './statements/statements.module';
 import { FinancialReleasesModule } from './financial_releases/financial_releases.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/challenge'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri:`${config.get<string>('MONGODB_URL')}:${config.get<string>('MONGODB_PORT')}/${config.get<string>('MONGODB_DATABASE')}`,
+      })
+    }),
     StatementsModule,
-    FinancialReleasesModule
+    FinancialReleasesModule,
+    ConfigModule.forRoot({ isGlobal: true })
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/statements/statements.service.ts
+++ src/statements/statements.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { FinancialReleases, FinancialReleasesDocument } from 'src/schemas/financial_releases.schema';
 import {dateSanitizer} from '../utils/sanitizer'
+import { format } from 'date-fns';
 @Injectable()
 export class StatementsService {
   constructor(@InjectModel(FinancialReleases.name) private readonly financialReleaseModel: Model<FinancialReleasesDocument>,){}
@@ -15,21 +16,32 @@ export class StatementsService {
     "$lte": new Date(dateAux.endDay)}}).exec();
     const statement = {};
     (await dbResult).forEach(element => {
-      const releaseDate = element.releaseDate.toString();
-      if (!statement[releaseDate]) {
-        statement[releaseDate]= {
+      const formattedDate = format(element.releaseDate, 'dd/MM/yyyy')
+      
+      if (!statement[formattedDate]) {
+        statement[formattedDate]= {
           "total": 0,
           "credito": [],
           "debito": []
       };
       }
       
-        statement[releaseDate].total = element.type === "credito" ?  statement[releaseDate].total + element.value : statement[releaseDate].total - element.value;
+        statement[formattedDate].total = element.type === "credito" ?  statement[formattedDate].total + element.value : statement[formattedDate].total - element.value;
         //@ts-ignore
-        statement[releaseDate][element.type].push({"date": element.created, 'value': element.value, "description": element.description});
-      
-      console.log(element);
+        statement[formattedDate][element.type].push({"date": format(element.created, 'dd/MM/yyyy - HH:mm'), 'value': element.value, "description": element.description});      
     });
+    console.log("Object.keys(statement).length", Object.keys(statement).length);
+    
+    if (Object.keys(statement).length > 1) {
+      statement["totalPeriodo"] = 0;
+      await Object.entries(statement).forEach((entry) => {
+        const [key, value] = entry;
+        if (key !== "totalPeriodo") {
+          //@ts-ignore
+          statement["totalPeriodo"] = statement["totalPeriodo"] + value.total;
+        }
+      });
+    }
 
     return statement;
 
